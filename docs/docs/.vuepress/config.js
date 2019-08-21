0fe4a66e32690b05ea4e86b3115c1398373acc2d
Andrew Welch <andrew@nystudio107.com>
2019-08-21 16:16:43 -0400

    Move docs to sub-folder
    Signed-off-by: Andrew Welch <andrew@nystudio107.com>

--- docs/docs/.vuepress/config.js
+++ docs/docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
     title: 'iCalendar Documentation',
     description: 'Documentation for the iCalendar plugin',
     base: '/docs/icalendar/',
-    dest: '../../../sites/nystudio107/web/docs/icalendar',
+    dest: '../../../../sites/nystudio107/web/docs/icalendar',
     themeConfig: {
         sidebar: [
             ['/', 'iCalendar plugin for Craft CMS 3.x'],
