5b3c9eb127d8fd58c822a1517d122bf1abf20ff1
Andrew Welch <andrew@keluli.local>
2022-03-27 13:46:26 -0400

    feat: Initial Craft CMS 4 compatibility

--- src/ICalendar.php
+++ src/ICalendar.php
@@ -52,7 +52,7 @@ class ICalendar extends Plugin
     /**
      * @var string
      */
-    public $schemaVersion = '1.0.0';
+    public string $schemaVersion = '1.0.0';
 
     // Public Methods
     // =========================================================================
